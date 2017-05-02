635be28c9d6630d205cc5ba9beedc3f14fe56059
Yann LM <yannlm2010@hotmail.fr>
2017-05-02 10:55:19 +0200

    Quelques changements style...

--- vues/personnel/param/general.php
+++ vues/personnel/param/general.php
@@ -18,10 +18,47 @@
 	</div>
   <div class="other-info">
   	<div class="service"><h3><b>Service : </b><i><?php echo $infos_user[4];?></i></h3></div>
+    <?php if(isset($_POST['change-ok'])){ 
+      echo '
+      <input type="text" name="" id="input1/(\w+)/\u\1/g" class="form-control input-pers" value="" required="required" pattern="" title="" placeholder="Service">
+      ';
+     }?>
     <div class="mail"><h3><b>Email: </b><i><?php echo $infos_user[5];?></i></h3></div>
+    <?php if(isset($_POST['change-ok'])){ 
+      echo '
+      <input type="text" name="" id="input1/(\w+)/\u\1/g" class="form-control input-pers" value="" required="required" pattern="" title="" placeholder="Service">
+      ';
+     }?>
     <div class="vehicule"><h3><b>Véhicule utilisé : </b><i><?php echo $user_vehicule[2]." - ".$user_vehicule[3]." - ".$user_vehicule[1];?></i></h3></div>
+    <?php if(isset($_POST['change-ok'])){ 
+      echo '
+      <input type="text" name="" id="input1/(\w+)/\u\1/g" class="form-control input-pers" value="" required="required" pattern="" title="" placeholder="Service">
+      ';
+     }?>
     <div class="immatriculation"><h3><b>Immatriculation : </b><i><?php echo $user_vehicule_infos[1];?></i></h3></div>
+    <?php if(isset($_POST['change-ok'])){ 
+      echo '
+      <input type="text" name="" id="input1/(\w+)/\u\1/g" class="form-control input-pers" value="" required="required" pattern="" title="" placeholder="Service">
+      ';
+     }?>
     <div class="km"><h3><b>Kilometrage du véhicule : </b><i><?php echo $user_vehicule_infos[4]." Km";?></i></h3></div>
+    <?php if(isset($_POST['change-ok'])){ 
+      echo '
+      <input type="text" name="" id="input1/(\w+)/\u\1/g" class="form-control input-pers" value="" required="required" pattern="" title="" placeholder="Service">
+      ';
+     }?>
+
+     <?php 
+     if(isset($_POST['change-ok'])){
+        echo '<input type="submit" class="btn btn-primary changer-info" value="Ok" name="change-ok-fini">';
+     } elseif(!isset($_POST['change-ok'])) {
+        echo '<form action="" method="post">';
+        echo '<input type="submit" class="btn btn-primary changer-info" value="Changer" name="change-ok">';
+        echo '</form>';
+     } else {
+
+     }
+    ?>
   </div>
   
 </div>
